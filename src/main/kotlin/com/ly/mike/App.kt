f4f0bbcff8dfe449c833eff86364fe0ae44952dd
Mikhail.L <skyalternative@gmail.com>
2019-05-14 21:24:21 +0300

    Moved to gradle groovy, openJDK javaFX support out of the box

--- src/main/kotlin/com/ly/mike/App.kt
+++ src/main/kotlin/com/ly/mike/App.kt
@@ -1,2 +1,15 @@
 package com.ly.mike
 
+import tornadofx.*
+
+class MyApp : App(MyView::class)
+
+class MyView : View() {
+    override val root = vbox {
+        button("some")
+    }
+}
+
+fun main(args: Array<String>) {
+    launch<MyApp>(args)
+}
